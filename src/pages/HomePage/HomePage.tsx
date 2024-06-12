8d9d72b4bc883e4bd5a8d9c6df0e84abbd070693
Anna4voit <137306965+Anna4voit@users.noreply.github.com>
2024-06-12 16:58:21 +0300

    update
    add useTheme (partially), themes css

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import SideBar from '../../components/Sidebar/Sidebar';
+import { useTheme } from '../../components/Hooks/useTheme';
 
 const HomePage = () => {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
   return (
-    <div className="xl:flex flex-row w-1440 my-0 mx-auto">
+    <div className="min-w-80 xl:flex flex-row min-w-1440 my-0 mx-auto">
       <div className="hidden  xl:flex">
         <SideBar />
       </div>
-      <div className="w-full">
+      <div className="min-w-80  xl:w-1180">
         <Header />
 
         <Outlet />
