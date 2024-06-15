bc9f51846d99de2094a751aadddde753c86b49ae
rango198 <137562811+rango198@users.noreply.github.com>
2024-06-15 23:04:13 +0300

    create_component_button

---
+++ src/components/Custom/CustomButton/types.ts
@@ -0,0 +1,8 @@
+import { IconProps } from '@src/helpers/icon/types';
+
+export interface IButtonProps {
+  children?: React.ReactNode;
+  className?: string; // Tailwind CSS classes
+  iconProp?: IconProps;
+  onClick: () => void;
+}
--- src/components/Custom/CustomModal/Modal.tsx
+++ src/components/Custom/CustomModal/Modal.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { CustomModalProps } from './types';
+import Button from '../CustomButton/Button';
 
 const Modal: React.FC<CustomModalProps> = ({ isVisible, onClose, children }) => {
   if (!isVisible) return null;
+
+  const className =
+    'inline-flex justify-center w-full sm:w-auto px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:text-sm';
+  const propIcon = {
+    id: 'plus',
+    width: 'w-6',
+    height: 'h-6',
+    color: 'text-white',
+  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-10 bg-gray-500 bg-opacity-25">
       <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all max-w-lg w-full">
         <div className="p-4">{children}</div>
         <div className="p-4 bg-gray-100 flex justify-end">
-          <button
-            onClick={onClose}
-            type="button"
-            className="inline-flex justify-center w-full sm:w-auto px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:text-sm"
-          >
+          <Button iconProp={propIcon} className={className} onClick={onClose}>
             Close
-          </button>
+          </Button>
         </div>
       </div>
     </div>
